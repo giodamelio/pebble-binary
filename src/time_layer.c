b769f04eb81a7c49fee46bb81feb061e4c93447b
Gio d'Amelio <giodamelio@gmail.com>
2015-03-14 12:01:45 -0700

    Show the real time

--- src/time_layer.c
+++ src/time_layer.c
@@ -1,6 +1,21 @@
 #include "binary.h"
 #include "time_layer.h"
 
+// Get the n'th bit from an int
+int get_bit(int decimal, int N){
+
+    // Shifting the 1 for N-1 bits
+    int constant = 1 << (N-1);
+
+    // if the bit is set, return 1
+    if( decimal & constant ){
+        return 1;
+    }
+
+    // If the bit is not set, return 0
+    return 0;
+}
+
 // Column width
 #define col_width 18
 #define base_height 104
@@ -55,47 +70,77 @@ int col5[4][2] = {
 
 // Draw the time
 void draw_time(Layer *my_layer, GContext* ctx) {
+    // Get the time
+    time_t temp = time(NULL); 
+    struct tm *tick_time = localtime(&temp);
+
+    // Put the time into an array of chars
+    char buffer[7];
+    strftime(buffer, 7, "%I%M%S", tick_time);
+
+    // Set the colors
+    graphics_context_set_stroke_color(ctx, GColorBlack);
+    graphics_context_set_fill_color(ctx, GColorBlack);
+
     // Draw column 0
     int i;
     for (i = 0; i < 2; i++) {
-        graphics_context_set_stroke_color(ctx, GColorBlack);
-        graphics_context_set_fill_color(ctx, GColorBlack);
-        graphics_fill_circle(ctx, GPoint(col0[i][0], col0[i][1]), radius);
+        int filled = get_bit(buffer[0] - '0', i + 1);
+        if (filled) {
+            graphics_fill_circle(ctx, GPoint(col0[i][0], col0[i][1]), radius);
+        } else {
+            graphics_draw_circle(ctx, GPoint(col0[i][0], col0[i][1]), radius);
+        }
     }
 
     // Draw column 1
     for (i = 0; i < 4; i++) {
-        graphics_context_set_stroke_color(ctx, GColorBlack);
-        graphics_context_set_fill_color(ctx, GColorBlack);
-        graphics_fill_circle(ctx, GPoint(col1[i][0], col1[i][1]), radius);
+        int filled = get_bit(buffer[1] - '0', i + 1);
+        if (filled) {
+            graphics_fill_circle(ctx, GPoint(col1[i][0], col1[i][1]), radius);
+        } else {
+            graphics_draw_circle(ctx, GPoint(col1[i][0], col1[i][1]), radius);
+        }
     }
 
     // Draw column 2
     for (i = 0; i < 3; i++) {
-        graphics_context_set_stroke_color(ctx, GColorBlack);
-        graphics_context_set_fill_color(ctx, GColorBlack);
-        graphics_fill_circle(ctx, GPoint(col2[i][0], col2[i][1]), radius);
+        int filled = get_bit(buffer[2] - '0', i + 1);
+        if (filled) {
+            graphics_fill_circle(ctx, GPoint(col2[i][0], col2[i][1]), radius);
+        } else {
+            graphics_draw_circle(ctx, GPoint(col2[i][0], col2[i][1]), radius);
+        }
     }
 
     // Draw column 3
     for (i = 0; i < 4; i++) {
-        graphics_context_set_stroke_color(ctx, GColorBlack);
-        graphics_context_set_fill_color(ctx, GColorBlack);
-        graphics_fill_circle(ctx, GPoint(col3[i][0], col3[i][1]), radius);
+        int filled = get_bit(buffer[3] - '0', i + 1);
+        if (filled) {
+            graphics_fill_circle(ctx, GPoint(col3[i][0], col3[i][1]), radius);
+        } else {
+            graphics_draw_circle(ctx, GPoint(col3[i][0], col3[i][1]), radius);
+        }
     }
 
     // Draw column 4
     for (i = 0; i < 3; i++) {
-        graphics_context_set_stroke_color(ctx, GColorBlack);
-        graphics_context_set_fill_color(ctx, GColorBlack);
-        graphics_fill_circle(ctx, GPoint(col4[i][0], col4[i][1]), radius);
+        int filled = get_bit(buffer[4] - '0', i + 1);
+        if (filled) {
+            graphics_fill_circle(ctx, GPoint(col4[i][0], col4[i][1]), radius);
+        } else {
+            graphics_draw_circle(ctx, GPoint(col4[i][0], col4[i][1]), radius);
+        }
     }
 
     // Draw column 5
     for (i = 0; i < 4; i++) {
-        graphics_context_set_stroke_color(ctx, GColorBlack);
-        graphics_context_set_fill_color(ctx, GColorBlack);
-        graphics_fill_circle(ctx, GPoint(col5[i][0], col5[i][1]), radius);
+        int filled = get_bit(buffer[5] - '0', i + 1);
+        if (filled) {
+            graphics_fill_circle(ctx, GPoint(col5[i][0], col5[i][1]), radius);
+        } else {
+            graphics_draw_circle(ctx, GPoint(col5[i][0], col5[i][1]), radius);
+        }
     }
 }
 
--- src/time_layer.h
+++ src/time_layer.h
@@ -3,6 +3,7 @@
 
 #include <pebble.h>
 
+int get_bit(int decimal, int N);
 void time_tick(struct tm *tick_time, TimeUnits units_changed);
 void draw_time(Layer *layer, GContext* ctx);
 
